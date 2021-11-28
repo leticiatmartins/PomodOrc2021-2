eb13b88863b54efd9a20ec145688e5e3c1e2c5c4
Patrick Anderson Carvalho Dos Santos <pandersomm@gmail.com>
2021-11-28 16:16:00 -0300

    Co-authored-by: Gustavo Kenzo Araki Takechi <gustavokenzo314@gmail.com> Co-authored-by: Maykon Júnio <maykonjuso@users.noreply.github.com>

---
+++ front/src/pages/components/Homepage/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Homepage.css'
+
+function Homepage () {
+    return(
+        <div className='homepageMenu'>
+            <h1 className='homepageTitle'>Homepage</h1>
+        </div>
+    )
+}
+
+export default Homepage
---
+++ front/src/pages/components/SideBar/SideBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as CgIcons from 'react-icons/cg'
+import './SideBar.css';
+
+function SideBar () {
+    return(
+        <div>
+            <>
+            <CgIcons.CgProfile />
+            </>
+
+        </div>
+    )
+};
+
+export default SideBar
