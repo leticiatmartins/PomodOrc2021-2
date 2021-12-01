86a457c787158dd26fec98646246d151304c57b5
leticiatmartins <leticiats02@gmail.com>
2021-12-01 20:04:08 -0300

    fizemos a homepage e criamos o relogio

--- front/src/pages/components/Homepage/Homepage.tsx
+++ front/src/pages/components/Homepage/Homepage.tsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import React from 'react'
 import './Homepage.css'
+import play from '../../../assets/play.svg';
+import replay from '../../../assets/replay.svg';
+
+
+const[min, setMin] = useState (10)
+const[seg, setSeg] = useState (0)
+
 
 function Homepage () {
     return(
         <div className='homepageMenu'>
-            <h1 className='homepageTitle'>Homepage</h1>
+            <h1 className='homepageTitle'>POMOD'ORC</h1>
+            <img className='play' src={play}></img>
+            <img className='replay' src={replay}></img>
+            <div className="homeMainText"/>
+            {`${min < 10 ? `0${min}` : min} : ${seg < 10 ? `0${seg}` :  seg}`}
         </div>
     )
 }
--- front/src/pages/components/SideBar/SideBar.tsx
+++ front/src/pages/components/SideBar/SideBar.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import * as CgIcons from 'react-icons/cg'
+import usuario from '../../../assets/usuario.svg';
+import alarm from '../../../assets/alarm.svg';
+import list from '../../../assets/list.svg';
 import './SideBar.css';
 
 function SideBar () {
     return(
-        <div>
-            <>
-            <CgIcons.CgProfile />
-            </>
+        <div className="Sidebar">         
+            <img className="usuario" src={usuario} />
+            <img className="alarm" src={alarm} />
+            <img className="list" src={list} />
 
         </div>
     )
 };
 
-export default SideBar
+export default SideBar;
