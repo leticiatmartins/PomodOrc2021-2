483ebf528e05d42b3cdcd90aa6ac55f642beb09a
leticiatmartins <leticiats02@gmail.com>
2021-12-02 16:00:55 -0300

    progrssBar

--- front/src/pages/components/Homepage/Homepage.tsx
+++ front/src/pages/components/Homepage/Homepage.tsx
@@ -1,23 +1,27 @@
+import React from 'react';
 import { useState } from 'react';
-import React from 'react'
-import './Homepage.css'
+import './Homepage.css';
 import play from '../../../assets/play.svg';
 import replay from '../../../assets/replay.svg';
 
-
-const[min, setMin] = useState (10)
-const[seg, setSeg] = useState (0)
-
-
 function Homepage () {
+    const[min, setMin] = useState (10);
+    const[seg, setSeg] = useState (0);
     return(
-        <div className='homepageMenu'>
-            <h1 className='homepageTitle'>POMOD'ORC</h1>
-            <img className='play' src={play}></img>
-            <img className='replay' src={replay}></img>
-            <div className="homeMainText"/>
-            {`${min < 10 ? `0${min}` : min} : ${seg < 10 ? `0${seg}` :  seg}`}
-        </div>
+        <>
+            <div className='homepageMenu'>
+                <h1 className='homepageTitle'>POMOD'ORC</h1>
+                <img className='play' src={play}></img>
+                <img className='replay' src={replay}></img>
+                <div className="homeMainText">
+                    {`${min < 10 ? `0${min}` : min} : ${seg < 10 ? `0${seg}` :  seg}`}
+                </div>
+            </div>
+            <div className='progressBarBack'>
+                <div className='progressBar'></div>
+            </div>
+
+        </>
     )
 }
 
