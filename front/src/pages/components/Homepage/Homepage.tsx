c5f9fb03d376a7b5df0a45ee782d13e977de17db
leticiatmartins <leticiats02@gmail.com>
2021-12-03 15:26:15 -0300

    barra funcionando

--- front/src/pages/components/Homepage/Homepage.tsx
+++ front/src/pages/components/Homepage/Homepage.tsx
@@ -8,50 +8,59 @@ import replay from '../../../assets/replay.svg';
 
 
 const Homepage = () => {
-    
 
-    const[min, setMin] = useState (10);
-    const[seg, setSeg] = useState (0);
-    const[barProgress, setbarProgress] = useState(100);
-    const[stoping, setStoping] = useState(false);
-    const[start, setStart] = useState(false);
-    const[toPause, setToPause] = useState(false)
+
+    const [min, setMin] = useState(25);
+    const [seg, setSeg] = useState(0);
+    const [fullTime, setFullTime] = useState(min * 60 + seg);
+    const [barProgress, setbarProgress] = useState(100);
+    const [stoping, setStoping] = useState(false);
+    const [start, setStart] = useState(false);
+    const [toPause, setToPause] = useState(false);
 
     const handleTime = () => {
 
-        if(toPause) {
+        if (toPause) {
             setToPause(false);
             return
         }
         setStart(true)
-        if(seg === 0) {
-            if(min === 0) {
-                return 
+        if (seg === 0) {
+            if (min === 0) {
+                return
             } else {
-                setMin(min-1)
+                setMin(min - 1)
                 setSeg(59)
             }
         } else {
             setTimeout(() => {
-                setSeg(seg-1);
+                setSeg(seg - 1);
             }, 1000)
         }
     }
 
 
     useEffect(() => {
-        if(start === true)
-        handleTime()
+        if (start === true)
+            handleTime()
+            handleBarProggress()
     }, [seg])
-    
+
     const reset = () => {
         pauseTime()
         setStoping(true)
         setTimeout(() => {
-            setMin(10)
+            setMin(25)
             setSeg(0)
+            setFullTime(60)
+            setbarProgress(100)
             setStoping(false)
         }, 1000)
+    }
+    const handleBarProggress = () => {
+        const time = min * 60 + seg
+        const progress = (time * 100) / fullTime
+        setbarProgress(progress)
     }
 
     const pauseTime = () => {
@@ -59,18 +68,18 @@ const Homepage = () => {
         setStart(false)
     }
 
-    return(
+    return (
         <>
             <div className='homepageMenu'>
                 <h1 className='homepageTitle'>POMOD'ORC</h1>
                 <img className='play' onClick={start ? pauseTime : handleTime} src={play}></img>
                 <img className='replay' onClick={reset} src={replay}></img>
                 <div className="homeMainText">
-                    {`${stoping ? "10" : min < 10 ? `0${min}` : min} : ${stoping ? "00" : seg < 10 ? `0${seg}` :  seg}`}
+                    {`${stoping ? "25" : min < 10 ? `0${min}` : min} : ${stoping ? "00" : seg < 10 ? `0${seg}` :  seg}`}
                 </div>
             </div>
             <div className='progressBarBack'>
-                <div className='progressBar'></div>
+                <div className='progressBar' style={{width: `${stoping ? 100: barProgress}%`}} ></div>
             </div>
 
         </>
